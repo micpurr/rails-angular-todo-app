462513a8f5cda4ac4ae8dc154e5cfef55f689aa5
Stanislav <so.stanislav@gmail.com>
2016-05-12 18:13:18 +0300

    add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context "check create user" do
+		it 'success create user' do
+			email = "email@gmail.com"
+			user = User.new(email: email, password:'111111')
+			user.save
+			expect(User.find_by_email(email)).to eq(user)
+		end
+	end
 end
