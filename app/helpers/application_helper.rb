a8aa8fd62aa01a9dffe3b304c6f5c4ad5ab5866a
Stanislav <so.stanislav@gmail.com>
2016-05-13 13:33:12 +0300

    add form size helper and test for it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,75 @@ module ApplicationHelper
 	def lc(word, transform_action='capitalize')
 		I18n.t(word).send(transform_action)
 	end
+
+	##
+	##  Current grid used bootstrap grid size
+	##
+	BOOTSTRAP_GRID_SIZE = 12 # or 16
+
+	##
+	##  helper for Calculate offset 
+	##  and size for selected size
+	##
+	def get_col_size(grid_name, grid_value, offset)
+		##
+		##  Array for store styles
+		##
+		css = []
+
+		##
+		##  Set grid_value as BOOTSTRAP_GRID_SIZE if set nil value
+		##
+		grid_value = BOOTSTRAP_GRID_SIZE if grid_value.blank?
+
+		##
+		##  Create base size for grid
+		##
+		css << "col-#{grid_name}-#{grid_value}"
+
+		##
+		##  If grid_value have correct value 
+		##  and offset is on then create offset size
+		##  and add style to array
+		##
+		if grid_value <= (BOOTSTRAP_GRID_SIZE - 2) && offset
+			css << "col-#{grid_name}-offset-#{(BOOTSTRAP_GRID_SIZE - grid_value)/2}"
+		end
+
+		##
+		##  Conver array of styles to string and terurn it
+		##
+		css.join(" ")
+	end
+
+	##
+	##  helper for creating css for form size
+	##
+	def form_size(size={}, offset=true)
+		##
+		##  Array for store styles
+		##
+		css = []
+
+		##
+		##  Default values for grid sizes
+		##
+		default_xs = size[:xs] || 12
+		default_sm = size[:sm] || 12
+		default_md = size[:md] || 10
+		default_lg = size[:lg] || 8
+
+		##
+		##  Create grid sizes
+		##
+		css << get_col_size('xs', default_xs, offset)
+		css << get_col_size('sm', default_sm, offset)
+		css << get_col_size('md', default_md, offset)
+		css << get_col_size('lg', default_lg, offset)
+
+		##
+		##  Conver array of styles to string and terurn it
+		##
+		css.join(" ")
+	end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -14,4 +14,55 @@ RSpec.describe ApplicationHelper, type: :helper do
 			expect(helper.lc('actions.new', :upcase)).to eq("NEW")
 		end
 	end
+
+	describe "form size calculations" do
+		describe "get_col_size return correct values" do
+			it "should return string" do
+				expect(helper.get_col_size(:xs, 10, true)).to be_instance_of(String)
+			end
+
+			it "should not return array" do
+				expect(helper.get_col_size(:xs, 10, true)).not_to be_instance_of(Array)
+			end
+
+			it "should return bootstrap default grid size" do
+				expect(helper.get_col_size(:xs, nil, true)).to include("12").or include('16')
+			end
+
+			it "should not return offset if grid_value > 10 and default bootstrap grid size == 12" do
+				expect(helper.get_col_size(:xs, 11, true)).not_to include("offset")
+			end
+
+			it "should not return offset if grid_value > 14 and default bootstrap grid size == 16" do
+				expect(helper.get_col_size(:xs, 15, true)).not_to include("offset")
+			end
+
+			it "should not return offset if offset is tern off" do
+				expect(helper.get_col_size(:xs, 10, false)).not_to include("offset")
+			end
+
+			it "should return offset" do
+				expect(helper.get_col_size(:xs, 10, true)).to include("offset")
+			end
+
+			it "should return correct grid_name" do
+				expect(helper.get_col_size(:xs, 10, true)).to include("xs")
+			end
+		end
+
+		describe "form_size return correct values" do
+			it "should return string" do
+				expect(helper.form_size).to be_instance_of(String)
+			end
+
+			it "should return changed default values" do
+				xs, sm, md, lg = 10, 8, 6, 4
+				expect(helper.form_size({xs: xs, sm: sm, md: md, lg: lg})).to include("col-xs-#{xs}").and include("col-sm-#{sm}").and include("col-md-#{md}").and include("col-lg-#{lg}")
+			end
+
+			it "should not return offset when it turns off" do
+				expect(helper.form_size({}, false)).not_to include("offset")
+			end
+		end
+	end
 end
