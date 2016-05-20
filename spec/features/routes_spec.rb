466ed671bc43c21667634c159b1f2aa2fc2be8f5
Stanislav <so.stanislav@gmail.com>
2016-05-20 20:40:46 +0300

    add routes spec

--- spec/features/routes_spec.rb
+++ spec/features/routes_spec.rb
@@ -1,9 +1,59 @@
 require 'rails_helper'
 
-# RSpec.feature 'routes', :js => true do
-# 	scenario 'login page' do
-# 		visit '/'
+RSpec.feature 'routes', :js => true do
+	before(:each) do
+		@user = User.create!(email: 'user@gmail.com', password: "111111")
+		@project = Project.create!(name: Faker::Lorem.sentence, user_id: @user.id)
+		@task = Task.create!(name: Faker::Lorem.sentence, project_id: @project.id)
+	end
 
-# 		expect(page).to have_content('login page')
-# 	end
-# end
+	scenario "home page" do
+		visit '/'
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'profile page' do
+		visit '/profile'
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'some task page' do
+		visit "/projects/#{@project.id}/tasks/#{@task.id}"
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'edit task page' do
+		visit "/projects/#{@project.id}/tasks/#{@task.id}/up"
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'new project page' do
+		visit '/projects/new'
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'edit project page' do
+		visit "/projects/#{@project.id}/edit"
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'fake url' do
+		visit '/fake/url'
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'login page' do
+		visit '/login'
+		expect(page).to have_selector('form[name=formLogin]')
+	end
+
+	scenario 'login page' do
+		visit '/'
+
+		find("a[href='/sign-up']").click
+
+		wait_for_angular
+
+		expect(page).to have_selector('form[name=formSighUp]')
+	end
+end
